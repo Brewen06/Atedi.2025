a20a8ca7ef90bc8ff328ddee989c2002f97a121c
Brewen06 <brewen68@ik.me>
2025-12-20 07:41:18 +0100

    sous-parties windows

--- src/Controller/InterventionController.php
+++ src/Controller/InterventionController.php
@@ -556,10 +556,8 @@ class InterventionController extends AbstractController
                         $interventionReport->setWindowsInstall($windowsInstalls);
                     }
 
-                    if ($request->request->has('windows-version-manual') && !empty($request->request->get('windows-version-manual'))) {
-                        $interventionReport->setWindowsVersion($request->request->get('windows-version-manual'));
-                    } elseif ($request->request->has('windows-version-selected')) {
-                        $interventionReport->setWindowsVersion($request->request->get('windows-version-selected'));
+                    if ($request->request->has('windows-version') && !empty($request->request->get('windows-version'))) {
+                        $interventionReport->setWindowsVersion($request->request->get('windows-version'));
                     }
 
                     $interventionReport->setStep($step + 1);
